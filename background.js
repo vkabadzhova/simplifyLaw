f42ac7b2a64dc41babf4683791df8d32477220bd
vkabadzhova <vkabadzhova@gmail.com>
2024-08-03 15:39:55 -0700

    Change icon when a text is selected to be explained

--- background.js
+++ background.js
@@ -9,10 +9,25 @@ chrome.runtime.onInstalled.addListener(() => {
   });
 });
 
+
+// Function to update the icon based on the presence of selectedText in local storage
+function updateIcon() {
+  chrome.storage.local.get('selectedText', function(result) {
+    if (result.selectedText) {
+      // Set the active icon
+      chrome.action.setIcon({ path: 'images/128_SimplifyLaw_Active.png' });
+    } else {
+      // Set the default icon
+      chrome.action.setIcon({ path: 'images/128_SimplifyLaw_Logo.png' });
+    }
+  });
+}
+
 chrome.contextMenus.onClicked.addListener((item, tab) => {
   if (item.menuItemId === 'selection') {
     chrome.storage.local.set({ selectedText: item.selectionText }, () => {
       console.log('selectedText is set to ' + item.selectionText);
+      updateIcon();
     });
   }
 });
